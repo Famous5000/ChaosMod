e634a858d2d63cab11109e3b58518cf5d79e8f8b
Logan Darklock <logandarklock@gmail.com>
2019-07-10 21:43:04 -0700

    Give Chaos Infuser (T0) UNCOMMON rating (yellow name)

--- src/main/java/items/ChaosInfuserT0ItemBlock.kt
+++ src/main/java/items/ChaosInfuserT0ItemBlock.kt
@@ -2,5 +2,11 @@ package com.famous5000.chaos.items
 
 import com.famous5000.chaos.blocks.ChaosInfuserT0Block
 import com.famous5000.chaos.classes.ItemRegistriedBlock
+import net.minecraft.item.EnumRarity
+import net.minecraft.item.ItemStack
 
-object ChaosInfuserT0ItemBlock : ItemRegistriedBlock(ChaosInfuserT0Block)
+object ChaosInfuserT0ItemBlock : ItemRegistriedBlock(ChaosInfuserT0Block) {
+	override fun getRarity(stack: ItemStack): EnumRarity {
+		return EnumRarity.UNCOMMON
+	}
+}
