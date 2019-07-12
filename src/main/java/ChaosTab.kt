6bfd3f4628d0a5e9c652462e0bc78054b774b59f
Logan Darklock <logandarklock@gmail.com>
2019-07-12 01:46:46 -0700

    Use modid for creative tab

--- src/main/java/ChaosTab.kt
+++ src/main/java/ChaosTab.kt
@@ -4,7 +4,7 @@ import com.famous5000.chaos.items.ChaosStarItem
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.ItemStack
 
-object ChaosTab : CreativeTabs(CreativeTabs.CREATIVE_TAB_ARRAY.size, "chaos") {
+object ChaosTab : CreativeTabs(CreativeTabs.CREATIVE_TAB_ARRAY.size, modid) {
 	private val icon = ItemStack(ChaosStarItem)
 
 	override fun getTabIconItem() = icon
