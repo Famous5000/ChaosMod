cb05c141910bdbf6dab14b7cc89c052b0b4b442a
Logan Darklock <logandarklock@gmail.com>
2019-07-13 06:05:31 -0700

    Add capacity to Chaos Infuser T0 for testing

--- src/main/java/tileentities/ChaosInfuserT0TileEntity.kt
+++ src/main/java/tileentities/ChaosInfuserT0TileEntity.kt
@@ -5,7 +5,7 @@ import net.minecraft.util.text.ITextComponent
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TextComponentString
 
-class ChaosInfuserT0TileEntity : ChaosEnergyUserTileEntity(ChaosTier.T0) {
+class ChaosInfuserT0TileEntity : ChaosEnergyUserTileEntity(ChaosTier.T0, chaosEnergyCapacity = 1000000) {
 	override fun getDisplayName(): ITextComponent? {
 		return TextComponentString("Chaos Infuser (Tier 0)").setStyle(Style().setColor(getChaosTier().rarity.rarityColor))
 	}
